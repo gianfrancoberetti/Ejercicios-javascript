ec9034237f65665e75f2039f28257da9368f4bec
Gianfranco Beretti <gianfrancogb.gb@gmail.com>
2023-02-22 12:11:32 -0300

    subiendo el ejercicio numero dos

---
+++ 02-minusculas.js
@@ -0,0 +1,8 @@
+let str = 'SION ES UN BB PEQUENO, ES EL PERRRITO DE LA CASA';
+
+function stringToLowerCase(string){ 
+    const newStr = string.toLowerCase();
+    console.log(newStr)
+}
+
+stringToLowerCase(str);
